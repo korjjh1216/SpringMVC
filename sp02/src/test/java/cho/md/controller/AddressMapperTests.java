0aac5e21cfe35ed011573202dda5d3bcf410c01c
korjjh1216 <korjjh1216@naver.com>
2021-04-07 18:33:24 +0900

    2021-04-07 Spring MVC Study

--- sp02/src/test/java/cho/md/controller/AddressMapperTests.java
+++ sp02/src/test/java/cho/md/controller/AddressMapperTests.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import cho.md.domain.Address;
 import cho.md.mapper.AddressMapper;
 import lombok.extern.log4j.Log4j;
 
@@ -18,9 +19,21 @@ public class AddressMapperTests {
 	@Autowired
 	private AddressMapper addressMapper;
 	
+//	@Test
+//	public void testList() {
+//		log.info("AddressMapperTests addressMapper:" + addressMapper);
+//		log.info("AddressMapperTests testList()" + addressMapper.list());
+//	}
+//	@Test
+//	public void testList() {
+//		Address address = new Address(-1,"힘든","셋팅",null);
+//		addressMapper.insert(address);
+//		log.info("##insert() 수행완료");
+//	}
 	@Test
-	public void testList() {
-		log.info("AddressMapperTests addressMapper:" + addressMapper);
-		log.info("AddressMapperTests testList()" + addressMapper.list());
+	public void testDelete() {
+		long seq = 12L;
+		addressMapper.delete(seq);
+		log.info("##delete() 수행완료");
 	}
 }
